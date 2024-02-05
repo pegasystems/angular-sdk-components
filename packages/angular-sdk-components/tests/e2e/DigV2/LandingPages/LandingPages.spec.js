62f1ca8f7c12f457d5fbdc490ecf1d6fd9428520
mohas22 <Sameer.Mohammed@pega.com>
2024-02-05 16:47:15 +0530

    updated to address flaky tests

--- packages/angular-sdk-components/tests/e2e/DigV2/LandingPages/LandingPages.spec.js
+++ packages/angular-sdk-components/tests/e2e/DigV2/LandingPages/LandingPages.spec.js
@@ -33,16 +33,22 @@ test.describe('E2E test', () => {
     const myWorkLandingPage = page.locator('mat-list-item > span:has-text("My Work")');
     await myWorkLandingPage.click();
 
-    await page.locator('input[id="mat-input-1"]').pressSequentially(caseID, { delay: 100 });
+    await expect(page.getByRole('row', { name: ' Case ID ' })).toBeVisible();
+
+    await page.locator('input[id="search"]').pressSequentially(caseID, { delay: 100 });
     expect(await (await page.locator(`button:has-text("${caseID}")`).textContent()).trim()).toBe(caseID);
 
-    // await page.locator(`button >> span:has-text(" ${caseID} ")`).click();
+    await page.getByRole('row', { name: ' Case ID ' }).click();
 
-    // /** Testing that the Case View has rendered */
-    // expect(await page.locator('div[id="current-caseID"]').textContent()).toBe(`DXIL-DIGV2-WORK ${caseID}`);
+    const table = page.locator('table[id="list-view"]');
+    await table.locator(`button:has-text("${caseID}")`).click();
+    /** Testing that the Case View has rendered */
+    expect(await page.locator('div[id="current-caseID"]').textContent()).toBe(`DXIL-DIGV2-WORK ${caseID}`);
 
-    // /** Testing that the Assignment has opened */
-    // expect(page.locator('mat-select[data-test-id="76729937a5eb6b0fd88c42581161facd"]')).toBeVisible();
+    /** Testing that the Assignment has opened */
+    expect(await page.locator('app-assignment')).toBeVisible();
+    const dropdown = page.locator('mat-select[data-test-id="76729937a5eb6b0fd88c42581161facd"]');
+    await expect(dropdown).toBeVisible();
   }, 10000);
   test('should login, create case and come back to Home landing page and run tests', async ({ page }) => {
     await common.login(config.config.apps.digv2.user.username, config.config.apps.digv2.user.password, page);
