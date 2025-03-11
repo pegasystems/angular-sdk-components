a2b59a256eebbeddd052c0a990c4ee2968d7ecff
Siva Rama Krishna <sivaramakrishna.tumma@in.pega.com>
2025-03-11 17:26:20 +0530

    fix: incorrect referenceList value in refresh api payload on opening dialog

--- packages/angular-sdk-components/src/lib/_components/template/default-form/default-form.component.ts
+++ packages/angular-sdk-components/src/lib/_components/template/default-form/default-form.component.ts
@@ -86,7 +86,7 @@ export class DefaultFormComponent extends FormTemplateBase implements OnInit, On
     //  normalize them
     const children = ReferenceComponent.normalizePConnArray(kids[0].getPConnect().getChildren());
 
-    const visibleChildren = children.filter(child => child !== undefined);
+    const visibleChildren = children?.filter(child => child !== undefined) || [];
 
     if (areViewsChanged(this.arChildren$, visibleChildren)) {
       this.arChildren$ = visibleChildren;
