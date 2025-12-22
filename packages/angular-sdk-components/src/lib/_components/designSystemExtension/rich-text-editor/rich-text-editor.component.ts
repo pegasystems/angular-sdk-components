f1268acca5ee388dbcdedd88154100ba6fbf2d91
sambhu <sambhu.ss@pega.com>
2025-12-22 14:55:35 +0530

    format issues fixed

--- packages/angular-sdk-components/src/lib/_components/designSystemExtension/rich-text-editor/rich-text-editor.component.ts
+++ packages/angular-sdk-components/src/lib/_components/designSystemExtension/rich-text-editor/rich-text-editor.component.ts
@@ -27,7 +27,7 @@ export class RichTextEditorComponent implements OnChanges {
   @Output() onChange: EventEmitter<any> = new EventEmitter();
 
   richText = new FormControl();
-  editorConfig: any = {};
+  editorConfig : any ={};
 
   ngOnChanges() {
     if (this.required) {
@@ -43,7 +43,7 @@ export class RichTextEditorComponent implements OnChanges {
     if (this.value) {
       this.richText.setValue(this.value);
     }
-
+      
     const themeElement = document.querySelector('.dark') || document.body;
     let textColor = getComputedStyle(themeElement).getPropertyValue('--mat-sys-on-surface').trim();
     if (!textColor) textColor = '#000000';
@@ -52,17 +52,15 @@ export class RichTextEditorComponent implements OnChanges {
       base_url: '/tinymce',
       suffix: '.min',
       menubar: false,
-      placeholder: this.placeholder, // Pass the input prop
+      placeholder: this.placeholder,
       statusbar: false,
       min_height: 130,
       plugins: ['lists', 'advlist', 'autolink', 'image', 'link', 'autoresize'],
       autoresize_bottom_margin: 0,
-
-      // Dynamic Toolbar logic
+      
       toolbar: this.disabled ? false : 'blocks | bold italic strikethrough | bullist numlist outdent indent | link image',
       toolbar_location: 'bottom',
-
-      // THE FIX: Dynamic Color + Original Fonts
+      
       content_style: `
         body { 
           font-family: Helvetica, Arial, sans-serif; 
@@ -71,7 +69,7 @@ export class RichTextEditorComponent implements OnChanges {
           background: transparent !important; 
         }
       `,
-
+      
       branding: false,
       paste_data_images: true,
       file_picker_types: 'image',
