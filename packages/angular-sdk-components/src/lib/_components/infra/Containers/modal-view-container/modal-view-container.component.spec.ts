6a4dc6ba58d20222c00bc42fe6d76f110f592f98
Siva Rama Krishna <sivaramakrishna.tumma@in.pega.com>
2026-09-24 13:14:04 +0530

    refactor(modal-view-container): simplify test setup and remove unused tests

--- packages/angular-sdk-components/src/lib/_components/infra/Containers/modal-view-container/modal-view-container.component.spec.ts
+++ packages/angular-sdk-components/src/lib/_components/infra/Containers/modal-view-container/modal-view-container.component.spec.ts
@@ -1,61 +1,24 @@
-import { ChangeDetectorRef } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ModalViewContainerComponent } from './modal-view-container.component';
 
 describe('ModalViewContainerComponent', () => {
   let component: ModalViewContainerComponent;
-  let markForCheckSpy: jasmine.Spy;
+  let fixture: ComponentFixture<ModalViewContainerComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ModalViewContainerComponent]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
-    (globalThis as any).PCore = {
-      getConstants: () => ({ PAGE: 'PAGE' })
-    };
-
-    markForCheckSpy = jasmine.createSpy('markForCheck');
-    const cdRef = { markForCheck: markForCheckSpy } as unknown as ChangeDetectorRef;
-
-    component = new ModalViewContainerComponent({} as any, cdRef, {} as any, new FormBuilder());
-    component.pConn$ = { getStateProps: () => ({}) } as any;
-    component.itemKey$ = 'app/modal_1';
-    component.stateProps$ = {};
+    fixture = TestBed.createComponent(ModalViewContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('surfaces httpMessages captured by the bridge as an urgent banner', () => {
-    component.angularPConnectData.httpMessages = ['Save failed'];
-
-    const banners = component.getBanners();
-
-    expect(banners.length).toBe(1);
-    expect(banners[0].messages).toEqual(['Save failed']);
-    expect(banners[0].variant).toBe('urgent');
-  });
-
-  it('produces no banner when there are no httpMessages', () => {
-    component.angularPConnectData.httpMessages = undefined;
-
-    expect(component.getBanners()).toEqual([]);
-  });
-
-  it('refreshes banners and flags change detection when they change', () => {
-    component.angularPConnectData.httpMessages = ['Save failed'];
-
-    component.refreshBanners();
-
-    expect(component.banners.length).toBe(1);
-    expect(markForCheckSpy).toHaveBeenCalled();
-  });
-
-  it('does not flag change detection when the banners are unchanged', () => {
-    component.banners = component.getBanners();
-    markForCheckSpy.calls.reset();
-
-    component.refreshBanners();
-
-    expect(markForCheckSpy).not.toHaveBeenCalled();
   });
 });
